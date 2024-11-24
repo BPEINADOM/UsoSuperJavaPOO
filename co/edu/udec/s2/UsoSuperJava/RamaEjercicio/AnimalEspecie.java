17c84b03a388d524175bafb045f649a469feb857
7502410037BillyPeinadoM <bpeinadom@unicartagena.edu.co>
2024-11-24 18:48:54 -0500

    Ejercicio 2 completado

---
+++ co/edu/udec/s2/UsoSuperJava/RamaEjercicio/AnimalEspecie.java
@@ -0,0 +1,18 @@
+import Domain.Model.Ejercicio2.Pez;
+
+public class AnimalEspecie {
+
+    public static void main(String[] args) {
+        
+        System.out.println("*****************7502410037******************");
+        System.out.println("********BILLY JEAN YER PEINADO MADERA********");
+        System.out.println("*********************************************");
+
+        Pez pez = new Pez("Pez Betta", "Agua Dulce");
+
+        pez.mostrarEspecie();
+
+        System.out.println("*********************************************");
+
+    }
+}
